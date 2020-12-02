0501ce729a62d57dcfe4e8730371d578503fab72
Kihyeon KWON <54575770+kihyeon04@users.noreply.github.com>
2020-12-02 22:40:08 +0900

    finished!

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const save = document.getElementById("jsSave")
 
 canvas.width = 700;
 canvas.height = 700;
-
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = "#2c2c2c"
+ctx.fillStyle = "#2c2c2c"
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
 
 
 function onMouseMove(event) {
@@ -45,17 +49,42 @@ function startPainting() {
 
 }
 
+function changeMode() {
+    if (filling) {
+        filling = false;
+        mode.innerText = 'Fill'
+    } else {
+        filling = true;
+        mode.innerText = 'Paint'
+    }
+}
+
+function fillCanvas() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    }
+}
+
+
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("mousedown", fillCanvas)
+}
+
+if (mode) {
+    mode.addEventListener("click", changeMode)
 }
 
 
 for (let i = 0; i < colors.length; i++){
     colors[i].addEventListener("click", (event) => {
         ctx.strokeStyle = event.target.style.backgroundColor
+        ctx.fillStyle = event.target.style.backgroundColor
     })
 }
 
@@ -64,9 +93,14 @@ range.addEventListener("mouseup", (event) => {
 })
 
 save.addEventListener("click", (event) => {
-    console.log(event)
+    let image = canvas.toDataURL()
+    let link = document.createElement("a")
+    link.href = image
+    link.download = 'download'
+    link.click()
 })
 
 mode.addEventListener("click", (event) => {
     console.log(event)
 })
+
